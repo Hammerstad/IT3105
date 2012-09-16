7970b957980c5e75b76ba33b49857738fa25f437
Eirik M Hammerstad <emhammerstad@gmail.com>
2012-09-16 20:01:56 +0200

    Updates to phase2 player.

--- src/ai/PlayerPhaseII.java
+++ src/ai/PlayerPhaseII.java
@@ -9,13 +9,16 @@ public class PlayerPhaseII extends PlayerInterface {
 
 	static double[][][][] preflopTable;
 	int noOpponents;
+	double willBetIfAbove;
 	PlayerPersonality personality;
+	double riskAversion = 0; // How greedy/risky is the player
 
 	public PlayerPhaseII(PlayerPersonality personality) {
 		if (preflopTable == null) {
 			preflopTable = new PreflopReader().read("");
 		}
 		this.personality = personality;
+		setriskAversion();
 	}
 
 	public PlayerPhaseII() {
@@ -23,86 +26,63 @@ public class PlayerPhaseII extends PlayerInterface {
 			preflopTable = new PreflopReader().read("");
 		}
 		this.personality = PlayerPersonality.getRandom();
+		setriskAversion();
+	}
+
+	private void setriskAversion() {
+		switch (personality) {
+		case GREEDY:
+			riskAversion = 0.850;
+			break;
+		case NORMAL:
+			riskAversion = 1.000;
+			break;
+		case RISKY:
+			riskAversion = 1.250;
+			break;
+		}
 	}
 
 	@Override
-	public void bet(Game game) {
+	public double bet(Game game, double toCall) {
+		noOpponents = game.activePlayers.size();
+		willBetIfAbove = Math.pow(0.75, noOpponents);
 		switch (game.state) {
 		case PREFLOP_BETTING:
-			preFlopBet(game);
-			break;
+			return preFlopBet(game, toCall);
 		case PRERIVER_BETTING:
-			preRiverBet(game);
-			break;
+			return postFlopBet(game, toCall);
 		case PRETURN_BETTING:
-			preTurnBet(game);
-			break;
+			return postFlopBet(game, toCall);
 		case FINAL_BETTING:
-			finalBet(game);
-			break;
+			return postFlopBet(game, toCall);
+		default:
+			return -1;
 		}
 	}
 
-	private void preFlopBet(Game game) {
+	private double preFlopBet(Game game, double toCall) {
 		// Do something based on preflop table
-		noOpponents = game.activePlayers.size();
-		int suitedCards = (hand[0].suit == hand[1].suit) ? 1 : 0; //Check if cards are suited
-		double preflopCalculation = preflopTable[suitedCards][noOpponents][hand[0].value][hand[1].value]; //Get preflop chances
-		double personalityWeightedDesicion = 0; //How greedy/risky is the player
-		switch (personality) {
-		case GREEDY:
-			personalityWeightedDesicion = 0.850;
-			break;
-		case NORMAL:
-			personalityWeightedDesicion = 1.000;
-			break;
-		case RISKY:
-			personalityWeightedDesicion = 1.250;
-			break;
-		}
-		double willBetIfAbove = Math.pow(0.75, noOpponents); //Bet if preflop chances times greedyness is above this
-		if(preflopCalculation * personalityWeightedDesicion < willBetIfAbove){
-			//FOLD
-		}else if(preflopCalculation * personalityWeightedDesicion > (willBetIfAbove + 0.1*personalityWeightedDesicion )){
-			//RAISE, WE GOT GOOD A HAND!
-		}else{
-			//CALL/CHECK
+		int suitedCards = (hand[0].suit == hand[1].suit) ? 1 : 0; // Check if cards are suited
+		double preflopCalculation = preflopTable[suitedCards][noOpponents][hand[0].value][hand[1].value]; // Get preflop chances
+
+		if (preflopCalculation * riskAversion < willBetIfAbove) {
+			return -1;
+		} else if (preflopCalculation * riskAversion > (willBetIfAbove + 0.1 * riskAversion)) {
+			return toCall + game.blinds * riskAversion;
+		} else {
+			return toCall;
 		}
 	}
 
-	private void preRiverBet(Game game) {
+	private double postFlopBet(Game game, double toCall) {
 		double handStrength = HandStrength.handstrength(getHand(), game.table, game.activePlayers.size());
-		switch (personality) {
-		case GREEDY:
-			break;
-		case NORMAL:
-			break;
-		case RISKY:
-			break;
-		}
-	}
-
-	private void preTurnBet(Game game) {
-		double handStrength = HandStrength.handstrength(getHand(), game.table, game.activePlayers.size());
-		switch (personality) {
-		case GREEDY:
-			break;
-		case NORMAL:
-			break;
-		case RISKY:
-			break;
-		}
-	}
-
-	private void finalBet(Game game) {
-		double handStrength = HandStrength.handstrength(getHand(), game.table, game.activePlayers.size());
-		switch (personality) {
-		case GREEDY:
-			break;
-		case NORMAL:
-			break;
-		case RISKY:
-			break;
+		if (handStrength * riskAversion < willBetIfAbove) {
+			return -1;
+		} else if (handStrength * riskAversion > (willBetIfAbove + 0.1 * riskAversion)) {
+			return toCall + game.blinds * riskAversion;
+		} else {
+			return toCall;
 		}
 	}
 }
