2514974d1bb00064f9116475574fdf446b9a12ce
Nicklas Utgaard <nutgaard@gmail.com>
2012-09-16 22:29:02 +0200

    Removed count from Context

--- src/ai/Context.java
+++ src/ai/Context.java
@@ -15,16 +15,16 @@ public class Context {
     public enum Action {
 
         CALL(0), RAISE(1), FOLD(2);
-        
         int i;
-        private Action(int i){
+
+        private Action(int i) {
             this.i = i;
         }
+
         public int getBucket() {
             return i;
         }
     }
-
     private int playerId;
     private GameState gameState;
     private int raises;
@@ -32,8 +32,7 @@ public class Context {
     private double potOdds;
     private Action action;
     private double handstrengthAvg = 0;
-    private int handstrengthCount = 0;
-    
+
     public static Context createContext(int playerId, GameState gameState, int raises, int players, double potOdds, Action action, double handStrength) {
         Context c = new Context();
         c.playerId = playerId;
@@ -43,7 +42,6 @@ public class Context {
         c.potOdds = potOdds;
         c.action = action;
         c.handstrengthAvg = handStrength;
-        c.handstrengthCount++;
         return c;
     }
 
@@ -89,65 +87,27 @@ public class Context {
         return playerId;
     }
 
-    public void setPlayerId(int playerId) {
-        this.playerId = playerId;
-    }
-
     public GameState getGameState() {
         return gameState;
-    }
-
-    public void setGameState(GameState gameState) {
-        this.gameState = gameState;
     }
 
     public int getRaises() {
         return raises;
     }
 
-    public void setRaises(int raises) {
-        this.raises = raises;
-    }
-
     public int getPlayers() {
         return players;
-    }
-
-    public void setPlayers(int players) {
-        this.players = players;
     }
 
     public double getPotOdds() {
         return potOdds;
     }
 
-    public void setPotOdds(double potOdds) {
-        this.potOdds = potOdds;
-    }
-
     public Action getAction() {
         return action;
-    }
-
-    public void setAction(Action action) {
-        this.action = action;
     }
 
     public double getHandstrengthAvg() {
         return handstrengthAvg;
     }
-
-    public void setHandstrengthAvg(double handstrengthAvg) {
-        this.handstrengthAvg = handstrengthAvg;
-    }
-
-    public int getHandstrengthCount() {
-        return handstrengthCount;
-    }
-
-    public void setHandstrengthCount(int handstrengthCount) {
-        this.handstrengthCount = handstrengthCount;
-    }
-    
-
 }
--- src/poker/GameState.java
+++ src/poker/GameState.java
@@ -6,5 +6,12 @@ package poker;
  */
 public enum GameState {
 
-    START, PREFLOP_BETTING, FLOP, PRETURN_BETTING, TURN, PRERIVER_BETTING, RIVER, FINAL_BETTING, SHOWDOWN;
+    START(0), PREFLOP_BETTING(1), FLOP(2), PRETURN_BETTING(3), TURN(4), PRERIVER_BETTING(5), RIVER(6), FINAL_BETTING(7), SHOWDOWN(9);
+    int i;
+    private GameState(int i){
+        this.i = i;
+    }
+    public int getBucket(){
+        return i;
+    }
 }
