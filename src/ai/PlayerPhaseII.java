497274e4d2e93436f2c37ae90a14d0d4456586ad
Eirik M Hammerstad <emhammerstad@gmail.com>
2012-09-16 20:59:39 +0200

    Try to

--- src/ai/PlayerPhaseII.java
+++ src/ai/PlayerPhaseII.java
@@ -12,7 +12,7 @@ public class PlayerPhaseII extends AbstractPlayer {
 	static double[][][][] preflopTable; // The preflop table
 	int noOpponents; // How many opponents left
 	double willBetIfAbove; // Will bet if above this (lower value for higher number of players)
-	double riskAversion = 0; // How greedy/risky is the player
+	double riskAversion = 1.000; // How greedy/risky is the player
 
 	/**
 	 * Default Phase 2 player.
@@ -86,7 +86,7 @@ public class PlayerPhaseII extends AbstractPlayer {
 	private double preFlopBet(Game game, double toCall) {
 		// Do something based on preflop table
 		int suitedCards = (hand[0].suit == hand[1].suit) ? 1 : 0; // Check if cards are suited
-		double preflopCalculation = preflopTable[suitedCards][noOpponents][hand[0].value-1][hand[1].value-1]; // Get preflop chances
+		double preflopCalculation = preflopTable[suitedCards][noOpponents][hand[0].value-2][hand[1].value-2]; // Get preflop chances
 
 		if (preflopCalculation * riskAversion < willBetIfAbove) {
 			return -1;
