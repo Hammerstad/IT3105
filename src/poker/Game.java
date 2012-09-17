bfc10d58780be9e738ff447893f9c836bc9a1537
Eirik M Hammerstad <emhammerstad@gmail.com>
2012-09-17 19:21:59 +0200

    Change in game constructor.

--- src/poker/Game.java
+++ src/poker/Game.java
@@ -226,9 +226,9 @@ public class Game {
 	 */
 	public static void main(String[] args) {
 		int NOF_GAMES = 1000;
-		int NOF_PLAYERS = 6;
-		Game game = new Game(NOF_PLAYERS);
-		out.writeLine("Creating new game, players: " + NOF_PLAYERS + " Rounds: " + NOF_GAMES);
+		AbstractPlayer[] players = new PlayerGenerator().checkList2();
+		Game game = new Game(players);
+		out.writeLine("Creating new game, players: " + players.length + " Rounds: " + NOF_GAMES);
 		for (int i = 0; i < NOF_GAMES; i++) {
 			game.setState(GameState.START);
 		}
