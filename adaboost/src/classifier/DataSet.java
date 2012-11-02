f2371edbedd4ed471a4bb90573b64c6680560225
Eirik M Hammerstad <emhammerstad@gmail.com>
2012-11-02 20:04:07 +0100

    Commenting and fixing in DataSet.

--- adaboost/src/classifier/DataSet.java
+++ adaboost/src/classifier/DataSet.java
@@ -4,40 +4,105 @@ import java.util.ArrayList;
 
 public class DataSet {
 
+	/**
+	 * All of the instances in this DataSet
+	 */
 	private Instance[] instances;
 
+	/**
+	 * The weight of this DataSet
+	 */
+	private double weight;
+
+	/**
+	 * Default constructor
+	 */
+	private DataSet() {
+		weight = 1.0;
+	}
+
+	/**
+	 * Creates a DataSet from an ArrayList containing double lists. The last element in the double lists is the class/category of the instance.
+	 * 
+	 * @param list
+	 *            - ArrayList with double lists, the instances - unweighed.
+	 */
 	public DataSet(ArrayList<double[]> list) {
+		this();
 		instances = new Instance[list.size()];
 		for (int i = 0; i < list.size(); i++) {
 			instances[i] = new Instance(list.get(i));
 		}
 	}
-	
-	private DataSet(Instance[] instances){
+
+	/**
+	 * Creates a DataSet from a list of instances.
+	 * @param instances
+	 */
+	private DataSet(Instance[] instances) {
+		this();
 		this.instances = instances;
 	}
-	
-	public int length(){
+
+	/**
+	 * The weight of the entire DataSet
+	 * @return the weight, [ 0.0 , 1.0 ]
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Sets the weight of the entire DataSet
+	 * @param weight - double [ 0.0 , 1.0 ] are you kind.
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * How many Instances there are in our DataSet.
+	 * @return length
+	 */
+	public int length() {
 		return instances.length;
 	}
-	
-	public Instance[] instances(){
+
+	/**
+	 * Returns an array of the Instances in this DataSet.
+	 * @return Instances
+	 */
+	public Instance[] getInstances() {
 		return instances;
 	}
-	
-	public Instance get(int i){
-		return instances[i];
+
+	/**
+	 * Returns a specific Instance
+	 * @param position - 0-indexed position
+	 * @return Instance
+	 */
+	public Instance get(int position) {
+		return instances[position];
 	}
-	
-	public DataSet subset(int from, int to){
-		Instance[] instances = new Instance[to-from];
-		System.arraycopy(this.instances, from, instances, 0, to-from);
+
+	/**
+	 * Returns a DataSet containing a subset of instances.</br> IMPORTANT: EXCLUDING LAST ELEMENT!
+	 * @param from - position of first element, 0-indexed
+	 * @param to - position of last element, 0-indexed, excluding
+	 * @return DataSet with a subset of the instances
+	 */
+	public DataSet subset(int from, int to) {
+		Instance[] instances = new Instance[to - from];
+		System.arraycopy(this.instances, from, instances, 0, to - from);
 		return new DataSet(instances);
 	}
-	
-	public String toString(){
+
+	/**
+	 * Overridden toString method. Prints all the instances.
+	 */
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		for(Instance element: instances){
+		for (Instance element : instances) {
 			sb.append(element).append('\n');
 		}
 		return sb.toString();
