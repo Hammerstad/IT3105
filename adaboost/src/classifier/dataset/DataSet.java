d19ca30e78251e948432c486da6a2e36b2242262
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2012-11-17 20:55:18 +0100

    Javadoc and other fixes in classifier.dataset

--- adaboost/src/classifier/dataset/DataSet.java
+++ adaboost/src/classifier/dataset/DataSet.java
@@ -6,160 +6,180 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author Eirik Mildestveit Hammerstad
+ * @author Nicklas Utgaard
+ */
 public class DataSet {
-    
-    /**
-     * The different classes contained in this dataset
-     */
-    private int[] classes;
-    
-    /**
-     * The different attribute values contained in this dataset
-     */
-    private Map<Integer, double[]> attributeValues;
-    /**
-     * All of the instances in this DataSet
-     */
-    private Instance[] instances;
 
+	/**
+	 * The different classes contained in this dataset
+	 */
+	private int[] classes;
 
-    /**
-     * Default constructor
-     */
-    private DataSet() {
-        
-    }
+	/**
+	 * The different attribute values contained in this dataset
+	 */
+	private Map<Integer, double[]> attributeValues;
+	/**
+	 * All of the instances in this DataSet
+	 */
+	private Instance[] instances;
 
-    /**
-     * Creates a DataSet from an ArrayList containing double lists. The last
-     * element in the double lists is the class/category of the instance.
-     *
-     * @param list - ArrayList with double lists, the instances - unweighed.
-     */
-    public DataSet(ArrayList<double[]> list) {
-        this();
-        instances = new Instance[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            instances[i] = new Instance(list.get(i), 1.0 / list.size());
-        }
-        this.attributeValues = new HashMap<>();
-    }
+	/**
+	 * Default constructor
+	 */
+	private DataSet() {
 
-    /**
-     * Creates a DataSet from a list of instances.
-     *
-     * @param instances
-     */
-    public DataSet(Instance[] instances) {
-        this();
-        this.instances = instances;
-        this.attributeValues = new HashMap<>();
-    }
+	}
 
-    /**
-     * Returns a list of the weights of all the instances.
-     * @return weight of all the instances
-     */
-    public double[] getInstanceWeights(){
-    	double[] list = new double[instances.length];
-    	for(int i = 0; i < instances.length; i++){
-    		list[i] = instances[i].getWeight();
-    	}
-    	return list;
-    }
-    
+	/**
+	 * Creates a DataSet from an ArrayList containing double lists. The last element in the double lists is the class/category of the instance.
+	 * 
+	 * @param list
+	 *            - ArrayList with double lists, the instances - unweighed.
+	 */
+	public DataSet(ArrayList<double[]> list) {
+		this();
+		instances = new Instance[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			instances[i] = new Instance(list.get(i), 1.0 / list.size());
+		}
+		this.attributeValues = new HashMap<>();
+	}
 
-    /**
-     * How many Instances there are in our DataSet.
-     *
-     * @return length
-     */
-    public int length() {
-        return instances.length;
-    }
+	/**
+	 * Creates a DataSet from a list of instances.
+	 * 
+	 * @param instances
+	 */
+	public DataSet(Instance[] instances) {
+		this();
+		this.instances = instances;
+		this.attributeValues = new HashMap<>();
+	}
 
-    /**
-     * Returns an array of the Instances in this DataSet.
-     *
-     * @return Instances
-     */
-    public Instance[] getInstances() {
-        return instances;
-    }
+	/**
+	 * Returns a list of the weights of all the instances.
+	 * 
+	 * @return weight of all the instances
+	 */
+	public double[] getInstanceWeights() {
+		double[] list = new double[instances.length];
+		for (int i = 0; i < instances.length; i++) {
+			list[i] = instances[i].getWeight();
+		}
+		return list;
+	}
 
-    /**
-     * Returns a specific Instance
-     *
-     * @param position - 0-indexed position
-     * @return Instance
-     */
-    public Instance get(int position) {
-        return instances[position];
-    }
+	/**
+	 * How many Instances there are in our DataSet.
+	 * 
+	 * @return length
+	 */
+	public int length() {
+		return instances.length;
+	}
 
-    /**
-     * Returns a DataSet containing a subset of instances.</br> IMPORTANT:
-     * EXCLUDING LAST ELEMENT!
-     *
-     * @param from - position of first element, 0-indexed
-     * @param to - position of last element, 0-indexed, excluding
-     * @return DataSet with a subset of the instances
-     */
-    public DataSet subset(int from, int to) {
-        Instance[] instances = new Instance[to - from];
-        System.arraycopy(this.instances, from, instances, 0, to - from);
-        return new DataSet(instances);
-    }
+	/**
+	 * Returns an array of the Instances in this DataSet.
+	 * 
+	 * @return Instances
+	 */
+	public Instance[] getInstances() {
+		return instances;
+	}
 
-    /**
-     * Overridden toString method. Prints all the instances.
-     */
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (Instance element : instances) {
-            sb.append(element).append('\n');
-        }
-        return sb.toString();
-    }
-    /**
-     * Return the different classes contained in this dataset
-     * @return int 
-     */
-    public int[] getClasses() {
-        if (this.classes == null) {
-            Set<Integer> found = new HashSet<>();
-            for (Instance i : instances) {
-                found.add(i.getCategory());
-            }
-            this.classes = new int[found.size()];
-            int index = 0;
-            for (Integer i : found) {
-                classes[index++] = i;
-            }
-        }
-        return this.classes;
-    }
-    public double[] getAttributeValues(int attribute) {
-        if (!this.attributeValues.containsKey(attribute)) {
-            Set<Double> found = new HashSet<>();
-            for (Instance i : instances) {
-                found.add(i.getAttributes()[attribute]);
-            }
-            double[] attributes = new double[found.size()];
-            int index = 0;
-            for (Double d : found) {
-                attributes[index++] = d;
-            }
-            this.attributeValues.put(attribute, attributes);
-        }
-        return this.attributeValues.get(attribute);
-    }
-    
-    public double totalWeights(){
-    	double weight = 0;
-    	for(Instance element : this.instances){
-    		weight += element.getWeight();
-    	}
-    	return weight;
-    }
+	/**
+	 * Returns a specific Instance
+	 * 
+	 * @param position
+	 *            - 0-indexed position
+	 * @return Instance
+	 */
+	public Instance get(int position) {
+		return instances[position];
+	}
+
+	/**
+	 * Returns a DataSet containing a subset of instances.</br> IMPORTANT: EXCLUDING LAST ELEMENT!
+	 * 
+	 * @param from
+	 *            - position of first element, 0-indexed
+	 * @param to
+	 *            - position of last element, 0-indexed, excluding
+	 * @return DataSet with a subset of the instances
+	 */
+	public DataSet subset(int from, int to) {
+		Instance[] instances = new Instance[to - from];
+		System.arraycopy(this.instances, from, instances, 0, to - from);
+		return new DataSet(instances);
+	}
+
+	/**
+	 * Overridden toString method. Prints all the instances.
+	 */
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (Instance element : instances) {
+			sb.append(element).append('\n');
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * Return the different classes contained in this dataset
+	 * 
+	 * @return int
+	 */
+	public int[] getClasses() {
+		if (this.classes == null) {
+			Set<Integer> found = new HashSet<>();
+			for (Instance i : instances) {
+				found.add(i.getCategory());
+			}
+			this.classes = new int[found.size()];
+			int index = 0;
+			for (Integer i : found) {
+				classes[index++] = i;
+			}
+		}
+		return this.classes;
+	}
+
+	/**
+	 * Returns an array with all the different values an attribute may have.
+	 * 
+	 * @param attribute
+	 *            - double[] with unique double values.
+	 * @return
+	 */
+	public double[] getAttributeValues(int attribute) {
+		if (!this.attributeValues.containsKey(attribute)) {
+			Set<Double> found = new HashSet<>();
+			for (Instance i : instances) {
+				found.add(i.getAttributes()[attribute]);
+			}
+			double[] attributes = new double[found.size()];
+			int index = 0;
+			for (Double d : found) {
+				attributes[index++] = d;
+			}
+			this.attributeValues.put(attribute, attributes);
+		}
+		return this.attributeValues.get(attribute);
+	}
+
+	/**
+	 * Returns the total weights of this dataSet.
+	 * 
+	 * @return double, should be close to 1.0.
+	 */
+	public double totalWeights() {
+		double weight = 0;
+		for (Instance element : this.instances) {
+			weight += element.getWeight();
+		}
+		return weight;
+	}
 }
--- adaboost/src/classifier/dataset/Instance.java
+++ adaboost/src/classifier/dataset/Instance.java
@@ -2,48 +2,108 @@ package classifier.dataset;
 
 import java.util.Arrays;
 
+/**
+ * @author Eirik Mildestveit Hammerstad
+ * @author Nicklas Utgaard
+ */
 public class Instance {
 
-    private final int category;
-    private final double[] attributes;
-    private double weight;
+	/**
+	 * The class of this instance
+	 */
+	private final int category;
 
-    public Instance(double[] instance) {
-        this(instance, 1.0);
-    }
+	/**
+	 * An array with the attributes of this instance.
+	 */
+	private final double[] attributes;
 
-    public Instance(double[] instance, double weight) {
-        attributes = new double[instance.length - 1];
-        System.arraycopy(instance, 0, attributes, 0, instance.length - 1);
-        category = (int) instance[instance.length - 1];
-        this.weight = weight;
-    }
+	/**
+	 * The current weight of this instance. Initiated to 1.0 if not specified.
+	 */
+	private double weight;
 
-    public Instance(Instance instance) {
-    	// No need to shallow-copy the attributes, we never change the attributes =)
+	/**
+	 * Creates an instance from a double array. The last element in the array becomes the category(class). <br/>
+	 * Sets the weight of this instance to 1.0.
+	 * 
+	 * @param instance
+	 *            - double array, last element will become category(class).
+	 */
+	public Instance(double[] instance) {
+		this(instance, 1.0);
+	}
+
+	/**
+	 * Creates an instance from a double array. The last element in the array becomes the category(class).
+	 * 
+	 * @param instance
+	 *            - double array, last element will become category(class).
+	 * @param weight
+	 *            - the weight of this instance.
+	 */
+	public Instance(double[] instance, double weight) {
+		attributes = new double[instance.length - 1];
+		System.arraycopy(instance, 0, attributes, 0, instance.length - 1);
+		category = (int) instance[instance.length - 1];
+		this.weight = weight;
+	}
+
+	/**
+	 * Creates an instance from another instance.
+	 * 
+	 * @param instance
+	 *            - instance to be copied.
+	 */
+	public Instance(Instance instance) {
+		// No need to shallow-copy the attributes, we never change the attributes =)
 		this.attributes = instance.attributes;
 		this.category = instance.category;
 		this.weight = instance.weight;
 	}
 
+	/**
+	 * Returns the weight of this instance.
+	 * 
+	 * @return
+	 */
 	public double getWeight() {
-        return weight;
-    }
+		return weight;
+	}
 
-    public double[] getAttributes() {
-        return attributes;
-    }
+	/**
+	 * Returns the attributes of this instance.
+	 * 
+	 * @return double[] instance
+	 */
+	public double[] getAttributes() {
+		return attributes;
+	}
 
-    public int getCategory() {
-        return category;
-    }
+	/**
+	 * Returns the category(class) of this instance.
+	 * 
+	 * @return int category
+	 */
+	public int getCategory() {
+		return category;
+	}
 
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
+	/**
+	 * Sets the weight of this instance.
+	 * 
+	 * @param weight
+	 *            - double weight
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
 
-    @Override
-    public String toString() {
-        return "Attr: " + Arrays.toString(attributes) + " \t Class: " + category + " \t Weight: " + weight;
-    }
+	@Override
+	/**
+	 * Prints out the attributes, class and weight of this Instance.
+	 */
+	public String toString() {
+		return "Attr: " + Arrays.toString(attributes) + " \t Class: " + category + " \t Weight: " + weight;
+	}
 }
