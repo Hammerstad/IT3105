a05193da46c18c8f3ce1f1233c6c9358c25c28ad
Nicklas Utgaard <nutgaard@gmail.com>
2012-11-17 20:13:40 +0100

    Changed to new version DTC

--- adaboost/src/classifier/decisiontree/DecisionTreeBuilder.java
+++ adaboost/src/classifier/decisiontree/DecisionTreeBuilder.java
@@ -4,9 +4,9 @@
  */
 package classifier.decisiontree;
 
-import classifier.dataset.DataSet;
 import classifier.IBuilder;
 import classifier.IClassifier;
+import classifier.dataset.DataSet;
 import classifier.dataset.Instance;
 import classifier.dataset.matcher.DataSetMatcher;
 import classifier.dataset.matcher.attribute.AttributeEqualsMatcher;
@@ -14,15 +14,14 @@ import classifier.dataset.matcher.matcher.CategoryMatcher;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import util.MathHelper;
 
 /**
  *
  * @author Nicklas
  */
 public class DecisionTreeBuilder extends IBuilder {
-
     public static int depth = -1;
-    double eps = 0.000000000001;
 
     @Override
     protected IClassifier generateHypothesis(DataSet ds) {
@@ -31,14 +30,15 @@ public class DecisionTreeBuilder extends IBuilder {
         for (int i = 0; i < numberOfAttributes; i++) {
             selectableAttributes.add(i);
         }
+//        System.out.println("Weigths: "+MathHelper.sum(ds.getInstanceWeights()));
         Node n = buildTree(ds, selectableAttributes, depth);
 //        printGraph(n);
         return new DecisionTreeClassifier(n);
     }
 
     private Node buildTree(DataSet ds, List<Integer> selectableAttributes, int depth) {
-        int chooseAttribute = chooseSplittingAttribute(ds, selectableAttributes);
-        if (chooseAttribute == -1 || depth == 0) {
+        int attribute = findMinEntropyIndex(ds, selectableAttributes);
+        if (attribute == -1 || depth == 0) {
             int[] classes = ds.getClasses();
             int[] found = new int[classes.length];
             int foundInd = 0;
@@ -60,92 +60,147 @@ public class DecisionTreeBuilder extends IBuilder {
             } else {
                 return new LeafNode(-1);
             }
-        }
-        if (isMapped(ds)) {
+        } else if (hasUniqueClass(ds)) {
             return new LeafNode(ds.get(0).getCategory());
         } else {
-            Node n = new AttributeNode(chooseAttribute);
-            double[] attributeValues = ds.getAttributeValues(chooseAttribute);
-            DataSet[] dss = new DataSet[attributeValues.length];
-            int dssInd = 0;
-            for (double attrValue : attributeValues) {
-                dss[dssInd] = DataSetMatcher.filter(ds, new AttributeEqualsMatcher(chooseAttribute, attrValue));
-                if (isMapped(dss[dssInd])) {
-                    n.addChild(new LeafNode(dss[dssInd].getClasses()[0]), attrValue);
+            Node n = new AttributeNode(attribute);
+            DataSet[] subsets = createSubSets(ds, attribute);
+            for (DataSet child : subsets) {
+                if (hasUniqueClass(child)) {
+                    n.addChild(new LeafNode(child.getClasses()[0]), child.get(0).getAttributes()[attribute]);
                 } else {
-                    n.addChild(buildTree(dss[dssInd], new LinkedList<>(selectableAttributes), depth - 1), attrValue);
+                    n.addChild(buildTree(child, new LinkedList<>(selectableAttributes), depth - 1), child.get(0).getAttributes()[attribute]);
                 }
-                dssInd++;
             }
             return n;
         }
     }
 
-    private boolean isMapped(DataSet ds) {
+    private static DataSet[] createSubSets(DataSet ds, int attribute) {
+        double[] attributeValues = ds.getAttributeValues(attribute);
+        DataSet[] subs = new DataSet[attributeValues.length];
+        for (int i = 0; i < subs.length; i++) {
+            subs[i] = DataSetMatcher.filter(ds, new AttributeEqualsMatcher(attribute, attributeValues[i]));
+        }
+        return subs;
+    }
+
+    private boolean hasUniqueClass(DataSet ds) {
         return ds.getClasses().length == 1;
     }
 
-    private int chooseSplittingAttribute(DataSet ds, List<Integer> selectableAttributes) {
-        if (ds.length() == 0 || selectableAttributes.isEmpty()) {
+    private static int findMinEntropyIndex(DataSet ds, List<Integer> selectableIndices) {
+        if (ds.length() == 0 || selectableIndices.isEmpty()) {
             return -1;
         }
-        double[] entropy = new double[selectableAttributes.size()];
-        double allWeights = 0;
-        for (Instance i : ds.getInstances()){
-            allWeights+=i.getWeight();
-        }
-//        System.out.println("AllWeights: "+allWeights);
-        for (int attributeIndex = 0; attributeIndex < selectableAttributes.size(); attributeIndex++) {
-            int attributeIndexD = selectableAttributes.get(attributeIndex);
-            double[] distinctValuesForAttribute = ds.getAttributeValues(attributeIndexD);
-            for (int a = 0, aL = distinctValuesForAttribute.length; a < aL; a++) {
-                double attributeValue = distinctValuesForAttribute[a];
-                DataSet DPVk = DataSetMatcher.filter(ds, new AttributeEqualsMatcher(attributeIndexD, attributeValue));
-                double branchWeight = 0;
-                for (Instance bi : DPVk.getInstances()){
-                    branchWeight+=bi.getWeight();
-                }
-                branchWeight+=Double.MIN_VALUE; //Just to avoid log(0)
-//                double PVk = DataSetMatcher.filter(ds, new AttributeEqualsMatcher(attributeIndexD, attributeValue)).length() / (ds.length() * 1.0);
-                double PVk = branchWeight/allWeights;
-                
-                entropy[attributeIndex] += PVk * log2(PVk);
-            }
-        }
-        for (int i = 0; i < entropy.length; i++) {
-            entropy[i] *= -1;
-        }
         double min = Double.MAX_VALUE;
-        int minIndT = -1;
-        for (int minInd = 0; minInd < entropy.length; minInd++) {
-//            System.out.println("Testing: "+entropy[maxInd]+" "+max);
-            if (entropy[minInd] < min) {
-                min = entropy[minInd];
-                minIndT = minInd;
+        int selectedIndex = -1;
+        for (Integer i : selectableIndices) {
+            double e = entropy(ds, i);
+            if (e < min) {
+                min = e;
+                selectedIndex = i;
             }
         }
-        System.out.println("Entropy: "+Arrays.toString(entropy));
-        return selectableAttributes.remove(minIndT);
+        selectableIndices.remove((Integer) selectedIndex);
+        System.out.println("Choosing "+selectedIndex);
+        return selectedIndex;
+    }
+
+    private static double entropy(DataSet ds, int splitAttribute) {
+        int[] classes = ds.getClasses();
+        double[] entropies = new double[ds.getAttributeValues(splitAttribute).length];
+        int entropesInd = 0;
+        double dataSetWeight = MathHelper.sum(ds.getInstanceWeights());
+        for (double attributeValues : ds.getAttributeValues(splitAttribute)) {
+            DataSet localds = DataSetMatcher.filter(ds, new AttributeEqualsMatcher(splitAttribute, attributeValues));
+            double localEntropy = 0;
+            double localWeight = MathHelper.sum(localds.getInstanceWeights());
+            for (int cls : classes) {
+                DataSet localSet = DataSetMatcher.filter(localds, new CategoryMatcher(cls));
+                double frac = localSet.length() / (localds.length() * 1.0);
+                localEntropy += -frac * log2(frac);
+            }
+//            entropies[entropesInd++] = ((localds.length() * 1.0) / ds.length()) * localEntropy;
+            entropies[entropesInd++] = (localWeight/dataSetWeight)*localEntropy;
+        }
+//        System.out.println(Arrays.toString(entropies));
+//        System.out.println("Entropy: " + MathHelper.sum(entropies)+" when splitting on "+splitAttribute);
+        return MathHelper.sum(entropies);
+    }
+
+    public enum Shape {
+
+        CIRCLE(1), STAR(2), DIAMOND(3);
+        int i;
+
+        private Shape(int i) {
+            this.i = i;
+        }
+
+        public int getValue() {
+            return this.i;
+        }
+    }
+
+    public enum Size {
+
+        BIG(1), SMALL(2);
+        int i;
+
+        private Size(int i) {
+            this.i = i;
+        }
+
+        public int getValue() {
+            return this.i;
+        }
+    }
+
+    public enum Color {
+
+        GREEN(1), BLUE(2), RED(3);
+        int i;
+
+        private Color(int i) {
+            this.i = i;
+        }
+
+        public int getValue() {
+            return this.i;
+        }
     }
 
     private static double log2(double d) {
+        if (d == 0) {
+            return 0;
+        }
         return Math.log(d) / Math.log(2);
     }
 
-    public static void printGraph(Node root) {
-        printGraph(root, 0);
-    }
+    public static void main(String[] args) {
 
-    private static void printGraph(Node root, int indent) {
-        if (root == null) {
-            return;
-        }
-        for (int i = 0; i < indent; i++) {
-            System.out.print(" ");
-        }
-        System.out.println(root.toString());
-        for (Node c : root.getChildren()) {
-            printGraph(c, indent + 4);
-        }
+        DataSet ds = new DataSet(new Instance[]{
+                    new Instance(new double[]{Shape.CIRCLE.i, Size.BIG.i, Color.GREEN.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.CIRCLE.i, Size.BIG.i, Color.GREEN.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.CIRCLE.i, Size.BIG.i, Color.BLUE.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.CIRCLE.i, Size.SMALL.i, Color.GREEN.i, 2}, 1.0 / 16),
+                    new Instance(new double[]{Shape.CIRCLE.i, Size.SMALL.i, Color.RED.i, 2}, 1.0 / 16),
+                    new Instance(new double[]{Shape.STAR.i, Size.BIG.i, Color.RED.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.STAR.i, Size.BIG.i, Color.RED.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.STAR.i, Size.BIG.i, Color.BLUE.i, 2}, 1.0 / 16),
+                    new Instance(new double[]{Shape.STAR.i, Size.BIG.i, Color.GREEN.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.STAR.i, Size.SMALL.i, Color.RED.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.STAR.i, Size.SMALL.i, Color.BLUE.i, 2}, 1.0 / 16),
+                    new Instance(new double[]{Shape.DIAMOND.i, Size.BIG.i, Color.GREEN.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.DIAMOND.i, Size.BIG.i, Color.BLUE.i, 1}, 1.0 / 16),
+                    new Instance(new double[]{Shape.DIAMOND.i, Size.BIG.i, Color.GREEN.i, 2}, 1.0 / 16),
+                    new Instance(new double[]{Shape.DIAMOND.i, Size.SMALL.i, Color.BLUE.i, 2}, 1.0 / 16),
+                    new Instance(new double[]{Shape.DIAMOND.i, Size.SMALL.i, Color.RED.i, 1}, 1.0 / 16)
+                });
+        
+        DecisionTreeBuilder.depth = 5;
+        DecisionTreeBuilder b = new DecisionTreeBuilder();
+        b.build(ds);
     }
 }
